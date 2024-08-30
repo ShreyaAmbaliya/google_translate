0613fbd2297a0cf2cdcd340fa33f8613e0da5489
Shreya <54749242+ShreyaAmbaliya@users.noreply.github.com>
2024-08-30 18:00:41 +0530

    Update string_extension.dart

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -2,7 +2,7 @@ import 'package:google_translate/components/google_translate.dart';
 
 extension Translate on String {
   // Translate your text from source to target language
-  Future<String> translate({
+  Future<Future<Map<String,dynamic>>> translate({
     String? sourceLanguage,
     String? targetLanguage,
   }) {
